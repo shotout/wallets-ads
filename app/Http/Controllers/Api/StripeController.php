3862924dce4de4eecd5d688a66d84340d06d39f4
shotout <hevaldo66@gmail.com>
2022-09-12 19:48:05 +0700

    Stripe Payment

---
+++ app/Http/Controllers/Api/StripeController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Stripe\StripeClient;
+use Stripe\Customer;
+
+class StripeController extends Controller
+
+{
+    public function index(Request $request)
+    {
+        try {
+            $stripe = new StripeClient(env('STRIPE_LIVE_API_KEY'));
+
+            //get token from request
+            $result = $stripe->tokens->create([
+                'card' => [
+                    'number' => $request->card_number,
+                    'exp_month' => $request->exp_month,
+                    'exp_year' => $request->exp_year,
+                    'cvc' => $request->cvc,
+                ],
+            ]);
+
+            //charge customer
+            $charge = new StripeClient(env('STRIPE_LIVE_API_KEY'));
+
+            $charge->charges->create([
+                'amount' => $request->amount,
+                'currency' => 'usd',
+                'source' => $result->id,
+                'description' => $request->description,
+            ]);
+
+            return response()->json( ['message' => 'Payment Success'], 200);
+
+        } catch (\Exception $e) {
+
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
