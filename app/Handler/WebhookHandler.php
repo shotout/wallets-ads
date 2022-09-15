d2a6b7e86c3d56323cb08999e11ddd5c78a3bb35
shotout <hevaldo66@gmail.com>
2022-09-15 11:30:50 +0700

    Update Webhook Stripe Payment

--- app/Handler/WebhookHandler.php
+++ app/Handler/WebhookHandler.php
@@ -6,6 +6,7 @@ use App\Jobs\SendConfirmEmail;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob;
 use Contentful\Delivery\Client as DeliveryClient;
 use App\Models\Blacklisted;
+use App\Models\Campaign;
 use App\Models\StripePayment;
 use App\Models\User;
 use Contentful\Management\Client;
@@ -23,7 +24,7 @@ class WebhookHandler extends ProcessWebhookJob
         if ($request->type === 'checkout.session.completed' && $request->data['object']['payment_status'] === 'paid') {
 
             $paymentid = StripePayment::where('stripe_id', $request->data['object']['id'])->first();
-            $campaign = $paymentid->campaign_id;
+            $campaign = Campaign::where('id', $paymentid->campaign_id)->first();
             
             $client = New Client(env('CONTENTFUL_MANAGEMENT_ACCESS_TOKEN'));
             $environment = $client->getEnvironmentProxy(env('CONTENTFUL_SPACE_ID'), 'master');
