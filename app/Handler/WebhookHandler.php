3545ecaccd82d8f6706e2b52e1c248c451b72965
shotout <hevaldo66@gmail.com>
2022-09-17 01:28:54 +0700

    Update Contentful Webhook

--- app/Handler/WebhookHandler.php
+++ app/Handler/WebhookHandler.php
@@ -11,6 +11,7 @@ use App\Models\StripePayment;
 use App\Models\User;
 use Contentful\Management\Client;
 use Illuminate\Http\Request;
+use Illuminate\Log\Logger;
 use Illuminate\Support\Facades\Log;
 
 class WebhookHandler extends ProcessWebhookJob 
@@ -20,7 +21,7 @@ class WebhookHandler extends ProcessWebhookJob
     public function handle(Request $request)
     {
         $data = $this->webhookCall->payload;
-        Log::webhook_log($data);
+        logger($data);
        
         if ($request->type === 'checkout.session.completed' && $request->data['object']['payment_status'] === 'paid') {
 
