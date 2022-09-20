6242eb653591540c022a09dd2b1d2ed260957fdd
shotout <hevaldo66@gmail.com>
2022-09-20 11:00:41 +0700

    Update Stripe Webhook

--- app/Handler/WebhookHandler.php
+++ app/Handler/WebhookHandler.php
@@ -32,27 +32,22 @@ class WebhookHandler extends ProcessWebhookJob
             $environment = $client->getEnvironmentProxy(env('CONTENTFUL_SPACE_ID'), 'master');
 
             $updatepayment = StripePayment::where('stripe_id', $request->data['object']['id'])->first();
-            $updatepayment->payment_status = '1';
+            $updatepayment->status = '1';
             $updatepayment->save();
 
             $entry = $environment->getEntry($campaign->entry_id);
             $entry->setField('paymentStatus', 'en-US', true);
             $entry->update();
-            $entry->publish();     
+            $entry->publish();
+
+            $updatestatus = StripePayment::where('stripe_id', $request->data['object']['id'])->first();
+            $updatestatus->status = '1';
+            $updatestatus->save();
+                       
+            return response()->json(['success' => true], 200);
         }
 
-        if($data['sys']['contentType']['sys']['id'] == 'users')
-                {
-                    //retrieve data from contentful
-                    $entry_id = $data['sys']['id'];
-                    
-                    //retrieve user from database
-                    $user = User::where('entry_id', $entry_id)->first();
-                    SendConfirmEmail::dispatch($user, 'register')->onQueue('apiCampaign');
-
-                    
-
-                }
+       
         
     }
 }
