9adb91beaa5ff32080d28e2e9d47677b01df6943
shotout <hevaldo66@gmail.com>
2022-09-20 14:16:44 +0700

    Update Campaign

--- app/Http/Controllers/Api/CampaignController.php
+++ app/Http/Controllers/Api/CampaignController.php
@@ -333,7 +333,7 @@ class CampaignController extends Controller
             return $campaign;
         });
 
-        //start upload to contenful
+        //start upload campaign to contenful
         UploadCampaignToContentful::dispatch($campaign);
                 
         return response()->json([
