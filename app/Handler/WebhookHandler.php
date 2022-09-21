8fe325be0a64dd28b9ba213247a6f432532731f6
shotout <hevaldo66@gmail.com>
2022-09-21 08:42:03 +0700

    Update Invoice Webhook

--- app/Handler/WebhookHandler.php
+++ app/Handler/WebhookHandler.php
@@ -13,7 +13,11 @@ use App\Models\User;
 use Contentful\Management\Client;
 use Illuminate\Http\Request;
 use Illuminate\Log\Logger;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class WebhookHandler extends ProcessWebhookJob
 {
@@ -58,10 +62,11 @@ class WebhookHandler extends ProcessWebhookJob
         }
 
 
-        if ($data['type'] == 'Entry') {
+        if ($data['sys']['type'] == 'Entry') {
 
             if ($data['sys']['contentType']['sys']['id'] == 'adsPage' && isset($data['fields']['invoiceFile'])) {
 
+                logger($data);
                 //get data campaign
                 $entry_id = $data['sys']['id'];
                 $campaign = Campaign::where('entry_id', $entry_id)->first();
@@ -73,19 +78,37 @@ class WebhookHandler extends ProcessWebhookJob
                     $payment_status = '1';
                 }
 
+                $invoice_file = $data['fields']['invoiceFile']['en-US']['sys']['id'];
+                        
+                $response = Http::get('https://cdn.contentful.com/spaces/m6gjbuid69la/environments/master/assets/'.$invoice_file.'?access_token='.env('CONTENTFUL_DELIVERY_TOKEN'));            
+                
+                logger($response->json());
+                        
+                $invoice_link = 'Https:'.$response['fields']['file']['url'];
+                $invoice_name = time().$response['fields']['file']['fileName'];
+                
+                $file = file_get_contents($invoice_link);
+                Storage::disk('public')->put('invoices/'.$invoice_name, $file);
+
+                $invoice_url ='/storage/invoices/'.$invoice_name;
+                
+
                 //save invoice data 
                 $newinvoice = new Invoice();
                 $newinvoice->campaign_id = $campaign->id;
+                $newinvoice->user_id = $campaign->user_id;
                 $newinvoice->invoice_number = $data['fields']['invoiceNumber']['en-US'];
                 $newinvoice->invoice_date = $data['fields']['invoiceDate']['en-US'];
                 $newinvoice->campaign_name = $data['fields']['campaignName']['en-US'];
                 $newinvoice->amount = $data['fields']['totalBudget']['en-US'];
                 $newinvoice->payment_method = $data['fields']['paymentMethod']['en-US'];
                 $newinvoice->payment_status = $payment_status;
+                $newinvoice->invoice_url = $invoice_url;
                 $newinvoice->save();
 
                 
             }
+            
         }
     }
 }
