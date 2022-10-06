adc31d11fa9a1dde7448d5c62046ef7982732c3f
shotout <hevaldo66@gmail.com>
2022-10-06 21:42:47 +0700

    Update parameter coupon

--- app/Handler/WebhookHandler.php
+++ app/Handler/WebhookHandler.php
@@ -38,193 +38,197 @@ class WebhookHandler extends ProcessWebhookJob
         $client = new Client(env('CONTENTFUL_MANAGEMENT_ACCESS_TOKEN'));
         $environment = $client->getEnvironmentProxy(env('CONTENTFUL_SPACE_ID'), 'master');
 
-        if ($data['type'] === 'checkout.session.completed' && $data['data']['object']['payment_status'] === 'paid') {
+        if (isset($data['type'])) {
+            if ($data['type'] === 'checkout.session.completed' && $data['data']['object']['payment_status'] === 'paid') {
 
-            $paymentid = StripePayment::where('stripe_id', $data['data']['object']['id'])->first();
-            $campaign = Campaign::where('id', $paymentid->campaign_id)->first();
+                $paymentid = StripePayment::where('stripe_id', $data['data']['object']['id'])->first();
+                $campaign = Campaign::where('id', $paymentid->campaign_id)->first();
 
-            $updatepayment = StripePayment::where('stripe_id', $data['data']['object']['id'])->first();
-            $updatepayment->status = '1';
-            $updatepayment->save();
+                $updatepayment = StripePayment::where('stripe_id', $data['data']['object']['id'])->first();
+                $updatepayment->status = '1';
+                $updatepayment->save();
 
-            UpdateStatusPayment::dispatch($campaign)->delay(now()->addSeconds(90));
-            $updatestatus = StripePayment::where('stripe_id', $request->data['object']['id'])->first();
-            $updatestatus->status = '1';
-            $updatestatus->save();
+                UpdateStatusPayment::dispatch($campaign)->delay(now()->addSeconds(90));
+                $updatestatus = StripePayment::where('stripe_id', $request->data['object']['id'])->first();
+                $updatestatus->status = '1';
+                $updatestatus->save();
 
-            return response()->json(['success' => true], 200);
+                return response()->json(['success' => true], 200);
+            }
         }
 
 
-        if ($data['sys']['type'] == 'Entry') {
+        if (isset($data['sys']['type'])) {
+            if ($data['sys']['type'] == 'Entry') {
 
-            if ($data['sys']['contentType']['sys']['id'] == 'users') {
-                //retrieve data from contentful
-                $entry_id = $data['sys']['id'];
-
-                //retrieve user from database
-                $user = User::where('entry_id', $entry_id)->first();
-                SendConfirmEmail::dispatch($user, 'register')->delay(Carbon::now()->addseconds(10));
-            }
-
-            if ($data['sys']['contentType']['sys']['id'] == 'adsPage') {
-
-                if (isset($data['fields']['invoiceFile'])) {
-                    logger($data);
-                    //get data campaign
+                if ($data['sys']['contentType']['sys']['id'] == 'users') {
+                    //retrieve data from contentful
                     $entry_id = $data['sys']['id'];
-                    $campaign = Campaign::where('entry_id', $entry_id)->first();
-                    $invoice = Invoice::where('campaign_id', $campaign->id)->first();
+
+                    //retrieve user from database
+                    $user = User::where('entry_id', $entry_id)->first();
+                    SendConfirmEmail::dispatch($user, 'register')->delay(Carbon::now()->addseconds(10));
+                }
+
+                if ($data['sys']['contentType']['sys']['id'] == 'adsPage') {
+
+                    if (isset($data['fields']['invoiceFile'])) {
+                        logger($data);
+                        //get data campaign
+                        $entry_id = $data['sys']['id'];
+                        $campaign = Campaign::where('entry_id', $entry_id)->first();
+                        $invoice = Invoice::where('campaign_id', $campaign->id)->first();
 
 
 
-                    if ($data['fields']['paymentStatus']['en-US'] == false) { {
-                            $payment_status = '0';
+                        if ($data['fields']['paymentStatus']['en-US'] == false) { {
+                                $payment_status = '0';
+                            }
+                        } else {
+                            $payment_status = '1';
                         }
-                    } else {
-                        $payment_status = '1';
+
+                        $invoice_file = $data['fields']['invoiceFile']['en-US']['sys']['id'];
+
+                        $response = Http::get('https://cdn.contentful.com/spaces/m6gjbuid69la/environments/master/assets/' . $invoice_file . '?access_token=' . env('CONTENTFUL_DELIVERY_TOKEN'));
+
+                        logger($response->json());
+
+                        $invoice_link = 'Https:' . $response['fields']['file']['url'];
+                        $invoice_name = $response['fields']['file']['fileName'];
+
+                        $file = file_get_contents($invoice_link);
+                        Storage::disk('public')->put('invoices/' . $invoice_name, $file);
+
+                        $invoice_url = '/storage/invoices/' . $invoice_name;
+
+                        if (empty($invoice)) {
+
+                            //save invoice data 
+                            $newinvoice = new Invoice();
+                            $newinvoice->campaign_id = $campaign->id;
+                            $newinvoice->user_id = $campaign->user_id;
+                            $newinvoice->invoice_number = $data['fields']['invoiceNumber']['en-US'];
+                            $newinvoice->invoice_date = $data['fields']['invoiceDate']['en-US'];
+                            $newinvoice->campaign_name = $data['fields']['campaignName']['en-US'];
+                            $newinvoice->amount = $data['fields']['totalBudget']['en-US'];
+                            $newinvoice->payment_method = $data['fields']['paymentMethod']['en-US'];
+                            $newinvoice->payment_status = $payment_status;
+                            $newinvoice->invoice_url = $invoice_url;
+                            $newinvoice->save();
+
+                            //send invoice email                
+                            $invoice = $newinvoice;
+                        }
+
+                        if ($invoice) {
+
+                            //update invoice data                     
+                            $invoice->campaign_id = $campaign->id;
+                            $invoice->user_id = $campaign->user_id;
+                            $invoice->invoice_number = $data['fields']['invoiceNumber']['en-US'];
+                            $invoice->invoice_date = $data['fields']['invoiceDate']['en-US'];
+                            $invoice->campaign_name = $data['fields']['campaignName']['en-US'];
+                            $invoice->amount = $data['fields']['totalBudget']['en-US'];
+                            $invoice->payment_method = $data['fields']['paymentMethod']['en-US'];
+                            $invoice->payment_status = $payment_status;
+                            $invoice->invoice_url = $invoice_url;
+                            $invoice->update();
+                        }
+
+
+                        if ($data['fields']['sendInvoiceEmail']['en-US'] == true) {
+
+                            SendInvoiceEmail::dispatch($invoice)->delay(Carbon::now()->addseconds(10));
+                        }
                     }
 
-                    $invoice_file = $data['fields']['invoiceFile']['en-US']['sys']['id'];
 
-                    $response = Http::get('https://cdn.contentful.com/spaces/m6gjbuid69la/environments/master/assets/' . $invoice_file . '?access_token=' . env('CONTENTFUL_DELIVERY_TOKEN'));
+                    if ($data['fields']['scheduledCampaign']['en-US'] == true) {
 
-                    logger($response->json());
+                        $entry_id = $data['sys']['id'];
+                        $campaign = Campaign::where('entry_id', $entry_id)->first();
 
-                    $invoice_link = 'Https:' . $response['fields']['file']['url'];
-                    $invoice_name = $response['fields']['file']['fileName'];
+                        $total_budget = $data['fields']['totalBudget']['en-US'];
+                        $total_sendout = Audience::where('campaign_id', $campaign->id)->sum('total_user');
 
-                    $file = file_get_contents($invoice_link);
-                    Storage::disk('public')->put('invoices/' . $invoice_name, $file);
+                        if ($campaign->is_scheduled == 0) {
+                            $campaign->is_scheduled = 1;
+                            $campaign->save();
 
-                    $invoice_url = '/storage/invoices/' . $invoice_name;
-
-                    if (empty($invoice)) {
-
-                        //save invoice data 
-                        $newinvoice = new Invoice();
-                        $newinvoice->campaign_id = $campaign->id;
-                        $newinvoice->user_id = $campaign->user_id;
-                        $newinvoice->invoice_number = $data['fields']['invoiceNumber']['en-US'];
-                        $newinvoice->invoice_date = $data['fields']['invoiceDate']['en-US'];
-                        $newinvoice->campaign_name = $data['fields']['campaignName']['en-US'];
-                        $newinvoice->amount = $data['fields']['totalBudget']['en-US'];
-                        $newinvoice->payment_method = $data['fields']['paymentMethod']['en-US'];
-                        $newinvoice->payment_status = $payment_status;
-                        $newinvoice->invoice_url = $invoice_url;
-                        $newinvoice->save();
-
-                        //send invoice email                
-                        $invoice = $newinvoice;
-                    }
-
-                    if ($invoice) {
-
-                        //update invoice data                     
-                        $invoice->campaign_id = $campaign->id;
-                        $invoice->user_id = $campaign->user_id;
-                        $invoice->invoice_number = $data['fields']['invoiceNumber']['en-US'];
-                        $invoice->invoice_date = $data['fields']['invoiceDate']['en-US'];
-                        $invoice->campaign_name = $data['fields']['campaignName']['en-US'];
-                        $invoice->amount = $data['fields']['totalBudget']['en-US'];
-                        $invoice->payment_method = $data['fields']['paymentMethod']['en-US'];
-                        $invoice->payment_status = $payment_status;
-                        $invoice->invoice_url = $invoice_url;
-                        $invoice->update();
-                    }
-
-
-                    if ($data['fields']['sendInvoiceEmail']['en-US'] == true) {
-
-                        SendInvoiceEmail::dispatch($invoice)->delay(Carbon::now()->addseconds(10));
+                            SendScheduleCampaign::dispatch($campaign, $total_budget, $total_sendout)->delay(Carbon::now()->addseconds(10));
+                        }
                     }
                 }
 
 
-                if ($data['fields']['scheduledCampaign']['en-US'] == true) {
+                if ($data['sys']['contentType']['sys']['id'] == 'promoCodes') {
+
+                    $entry_id = $data['sys']['id'];
+
+                    $promo = Voucher::where('entry_id', $entry_id)->first();
+
+                    if ($promo) {
+                        $promo->code = $data['fields']['promoCode']['en-US'];
+                        $promo->coupon_id = $data['fields']['stripeCouponId']['en-US'];
+                        $promo->type = 1;
+                        $promo->value = $data['fields']['couponAmount']['en-US'];
+                        $promo->min_budget = $data['fields']['minimalSpent']['en-US'];
+                        $promo->update();
+                    } else {
+                        $newcoupon = new Voucher();
+                        $newcoupon->entry_id = $entry_id;
+                        $newcoupon->code = $data['fields']['promoCode']['en-US'];
+                        $newcoupon->coupon_id = $data['fields']['stripeCouponId']['en-US'];
+                        $newcoupon->type = 1;
+                        $newcoupon->value = $data['fields']['couponAmount']['en-US'];
+                        $newcoupon->min_budget = $data['fields']['minimalSpent']['en-US'];
+                        $newcoupon->save();
+                    }
+                }
+            }
+
+
+            if ($data['sys']['type'] == 'DeletedEntry') {
+
+                if ($data['sys']['contentType']['sys']['id'] == 'adsPage') {
+
 
                     $entry_id = $data['sys']['id'];
                     $campaign = Campaign::where('entry_id', $entry_id)->first();
 
-                    $total_budget = $data['fields']['totalBudget']['en-US'];
-                    $total_sendout = Audience::where('campaign_id', $campaign->id)->sum('total_user');
-
-                    if ($campaign->is_scheduled == 0) {
-                        $campaign->is_scheduled = 1;
+                    if ($campaign) {
+                        $campaign->is_deleted = 1;
                         $campaign->save();
 
-                        SendScheduleCampaign::dispatch($campaign, $total_budget, $total_sendout)->delay(Carbon::now()->addseconds(10));
+                        $audiences = Audience::where('campaign_id', $campaign->id)->get();
+
+                        foreach ($audiences as $audience) {
+
+                            $entry_audience = $environment->getEntry($audience->entry_id);
+                            $entry_audience->unpublish();
+                            $entry_audience->delete();
+                        }
                     }
                 }
-            }
 
+                if ($data['sys']['contentType']['sys']['id'] == 'users') {
 
-            if ($data['sys']['contentType']['sys']['id'] == 'promoCodes') {
+                    $entry_id = $data['sys']['id'];
+                    $user = User::where('entry_id', $entry_id)->first();
 
-                $entry_id = $data['sys']['id'];
-
-                $promo = Voucher::where('entry_id', $entry_id)->first();
-
-                if ($promo) {
-                    $promo->code = $data['fields']['promoCode']['en-US'];
-                    $promo->coupon_id = $data['fields']['stripeCouponId']['en-US'];
-                    $promo->type = 1;
-                    $promo->value = $data['fields']['couponAmount']['en-US'];
-                    $promo->min_budget = $data['fields']['minimalSpent']['en-US'];
-                    $promo->update();
-                } else {
-                    $newcoupon = new Voucher();
-                    $newcoupon->entry_id = $entry_id;
-                    $newcoupon->code = $data['fields']['promoCode']['en-US'];
-                    $newcoupon->coupon_id = $data['fields']['stripeCouponId']['en-US'];
-                    $newcoupon->type = 1;
-                    $newcoupon->value = $data['fields']['couponAmount']['en-US'];
-                    $newcoupon->min_budget = $data['fields']['minimalSpent']['en-US'];
-                    $newcoupon->save();
-                }
-            }
-        }
-
-
-        if ($data['sys']['type'] == 'DeletedEntry') {
-
-            if ($data['sys']['contentType']['sys']['id'] == 'adsPage') {
-
-
-                $entry_id = $data['sys']['id'];
-                $campaign = Campaign::where('entry_id', $entry_id)->first();
-
-                if ($campaign) {
-                    $campaign->is_deleted = 1;
-                    $campaign->save();
-
-                    $audiences = Audience::where('campaign_id', $campaign->id)->get();
-
-                    foreach ($audiences as $audience) {
-
-                        $entry_audience = $environment->getEntry($audience->entry_id);
-                        $entry_audience->unpublish();
-                        $entry_audience->delete();
+                    if ($user) {
+                        $user->delete();
                     }
                 }
-            }
 
-            if ($data['sys']['contentType']['sys']['id'] == 'users') {
+                if ($data['sys']['contentType']['sys']['id'] == 'promoCodes') {
 
-                $entry_id = $data['sys']['id'];
-                $user = User::where('entry_id', $entry_id)->first();
+                    $coupon = Voucher::where('entry_id', $data['sys']['id'])->first();
 
-                if ($user) {
-                    $user->delete();
-                }
-            }
-
-            if ($data['sys']['contentType']['sys']['id'] == 'promoCodes') {
-
-                $coupon = Voucher::where('entry_id', $data['sys']['id'])->first();
-
-                if ($coupon) {
-                    $coupon->delete();
+                    if ($coupon) {
+                        $coupon->delete();
+                    }
                 }
             }
         }
