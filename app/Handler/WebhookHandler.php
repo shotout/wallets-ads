f29a8252b762509268c81b15bb3bd484c02751d6
shotout <hevaldo66@gmail.com>
2022-10-06 19:11:51 +0700

    Update Contentful

--- app/Handler/WebhookHandler.php
+++ app/Handler/WebhookHandler.php
@@ -6,6 +6,7 @@ use App\Jobs\SendCampaignNotificationEmail;
 use App\Jobs\SendConfirmEmail;
 use App\Jobs\SendInvoiceEmail;
 use App\Jobs\SendScheduleCampaign;
+use App\Jobs\UpdateStatusPayment;
 use App\Models\Audience;
 use Spatie\WebhookClient\Jobs\ProcessWebhookJob;
 use Contentful\Delivery\Client as DeliveryClient;
@@ -37,23 +38,16 @@ class WebhookHandler extends ProcessWebhookJob
         $client = new Client(env('CONTENTFUL_MANAGEMENT_ACCESS_TOKEN'));
         $environment = $client->getEnvironmentProxy(env('CONTENTFUL_SPACE_ID'), 'master');
 
-        if ($request->type === 'checkout.session.completed' && $request->data['object']['payment_status'] === 'paid') {
+        if ($data['type'] === 'checkout.session.completed' && $data['data']['object']['payment_status'] === 'paid') {
 
-            $paymentid = StripePayment::where('stripe_id', $request->data['object']['id'])->first();
+            $paymentid = StripePayment::where('stripe_id', $data['data']['object']['id'])->first();
             $campaign = Campaign::where('id', $paymentid->campaign_id)->first();
 
-            $client = new Client(env('CONTENTFUL_MANAGEMENT_ACCESS_TOKEN'));
-            $environment = $client->getEnvironmentProxy(env('CONTENTFUL_SPACE_ID'), 'master');
-
-            $updatepayment = StripePayment::where('stripe_id', $request->data['object']['id'])->first();
+            $updatepayment = StripePayment::where('stripe_id', $data['data']['object']['id'])->first();
             $updatepayment->status = '1';
             $updatepayment->save();
 
-            $entry = $environment->getEntry($campaign->entry_id);
-            $entry->setField('paymentStatus', 'en-US', true);
-            $entry->update();
-            $entry->publish();
-
+            UpdateStatusPayment::dispatch($campaign)->delay(now()->addSeconds(90));
             $updatestatus = StripePayment::where('stripe_id', $request->data['object']['id'])->first();
             $updatestatus->status = '1';
             $updatestatus->save();
---
+++ app/Jobs/UpdateStatusPayment.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use Contentful\Management\Client;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateStatusPayment implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    private $campaign;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($campaign)
+    {
+        $this->campaign = $campaign;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $client = new Client(env('CONTENTFUL_MANAGEMENT_ACCESS_TOKEN'));
+        $environment = $client->getEnvironmentProxy(env('CONTENTFUL_SPACE_ID'), 'master');
+
+        $entry = $environment->getEntry($this->Campaign->entry_id);
+        $entry->setField('paymentStatus', 'en-US', true);
+        $entry->update();
+        $entry->publish();
+    }
+}
