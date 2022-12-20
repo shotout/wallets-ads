d355732b68b995b8f9568af4a3b85686c4da215f
shotout <hevaldo66@gmail.com>
2022-12-20 13:48:29 +0700

    update campaign

--- app/Http/Controllers/Api/CampaignController.php
+++ app/Http/Controllers/Api/CampaignController.php
@@ -586,7 +586,7 @@ class CampaignController extends Controller
                     $media->type = "ads_logo";
                 }
 
-                if ($request->ads_page_logo->isImage()) {
+                if ($request->ads_page_logo->isImage() == true) {
                     $filename = uniqid();
                     $fileExt = $request->ads_page_logo->getClientOriginalExtension();
                     $fileNameToStore = $filename . '_' . time() . '.' . $fileExt;
