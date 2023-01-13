af6f9b5386f05d02059e481025020fa0640a96ad
shotout <hevaldo66@gmail.com>
2023-01-13 09:29:39 +0700

    update upload campaign

--- app/Jobs/UploadCampaignToContentful.php
+++ app/Jobs/UploadCampaignToContentful.php
@@ -52,7 +52,7 @@ class UploadCampaignToContentful implements ShouldQueue
         $environment = $client->getEnvironmentProxy(env('CONTENTFUL_SPACE_ID'), 'master');
 
         //if record exist
-        if ($campaign->entry_id = 0 || $campaign->entry_id = null) {
+        if ($campaign->entry_id != 0 || $campaign->entry_id != null) {
             $delete_campaign = $environment->getEntry($campaign->entry_id);
             $delete_campaign->unpublish();
             $delete_campaign->delete();
