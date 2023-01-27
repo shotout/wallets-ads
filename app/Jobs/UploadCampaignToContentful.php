fc3a5753c2ea8b2565361b69195a8572798f17cf
shotout <hevaldo66@gmail.com>
2023-01-27 10:24:57 +0700

    adjust upload audience file

--- app/Jobs/UploadCampaignToContentful.php
+++ app/Jobs/UploadCampaignToContentful.php
@@ -194,25 +194,27 @@ class UploadCampaignToContentful implements ShouldQueue
                 } else {
                     $package = "Upload Own Audience Targeting";
 
-                    $file = new \Contentful\Core\File\RemoteUploadFile(
-                        $url_file->original_name,
-                        'xlsx/xls/csv',
-                        env("APP_URL") . $url_file->url
-                    );
+                    if ($url_file != NULL and $url_file != '') {
+                        $file = new \Contentful\Core\File\RemoteUploadFile(
+                            $url_file->original_name,
+                            'xlsx/xls/csv',
+                            env("APP_URL") . $url_file->url
+                        );
 
-                    $asset_file = new Asset();
-                    $asset_file->setTitle('en-US', 'Audience file of ' . $campaign->name);
-                    $asset_file->setFile('en-US', $file);
+                        $asset_file = new Asset();
+                        $asset_file->setTitle('en-US', 'Audience file of ' . $campaign->name);
+                        $asset_file->setFile('en-US', $file);
 
-                    //process file
-                    $environment->create($asset_file);
-                    $asset_file_id = $asset_file->getId();
-                    $asset_file = $environment->getAsset($asset_file_id);
-                    $asset_file->process('en-US');
+                        //process file
+                        $environment->create($asset_file);
+                        $asset_file_id = $asset_file->getId();
+                        $asset_file = $environment->getAsset($asset_file_id);
+                        $asset_file->process('en-US');
+                    }
                 }
 
-                
-                
+
+
 
                 $adtext1 = ads::where('id', $ad->id)->get()->toArray();
                 $adtext1 = json_decode($adtext1[0]['description'], true);
