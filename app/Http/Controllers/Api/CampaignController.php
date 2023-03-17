cc075ca6d61c3baf6fd6c533ffcc05d52541247d
shotout <hevaldo66@gmail.com>
2023-03-17 11:28:48 +0700

    fix cancel campaign

--- app/Http/Controllers/Api/CampaignController.php
+++ app/Http/Controllers/Api/CampaignController.php
@@ -460,7 +460,7 @@ class CampaignController extends Controller
 
             if ($request->has('campaign_audiences') && count($request->campaign_audiences) > 0) {
                 // Audience::where('campaign_id', $campaign->id)->delete();
-
+                $id_campaign= $campaign->id;
                 foreach ($request->campaign_audiences as $i => $audience) {
                     $audience = (object) $audience;
 
@@ -507,7 +507,7 @@ class CampaignController extends Controller
                         if ($checkaud == null) {
 
                             $adc = new Audience;
-                            $adc->campaign_id = $request->campaign_id;
+                            $adc->campaign_id = $id_campaign;
                             if (isset($audience->fe_id)) {
                                 $adc->fe_id = $audience->fe_id;
                             }
