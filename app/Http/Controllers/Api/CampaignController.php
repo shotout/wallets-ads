56d0cf913d3b6d444d2be6e67944e6bcf2e84ae5
shotout <hevaldo66@gmail.com>
2023-03-17 12:50:09 +0700

    fix cancel campaign

--- app/Http/Controllers/Api/CampaignController.php
+++ app/Http/Controllers/Api/CampaignController.php
@@ -771,19 +771,22 @@ class CampaignController extends Controller
                         $newads->description = $ads->description;
                         $newads->save();
 
-                        $audcheck = Audience::where('selected_fe_id', $ads->audience_id)->first();
+                        foreach ($ads->audience_id as $adc_id) {
 
-                        $newaud = new Audience;
-                        $newaud->campaign_id = $campaign->id;
-                        $newaud->ads_id = $newads->id;
-                        $newaud->fe_id = $audcheck->fe_id;
-                        $newaud->selected_fe_id = $ads->audience_id;
-                        $newaud->name = $audcheck->name;
-                        $newaud->description = $audcheck->description;
-                        $newaud->price = $audcheck->price;
-                        $newaud->price_airdrop = $audcheck->price_airdrop;
-                        $newaud->total_user = $audcheck->total_user;
-                        $newaud->save(); 
+                            $audcheck = Audience::where('selected_fe_id', $ads->audience_id)->first();
+
+                            $newaud = new Audience;
+                            $newaud->campaign_id = $campaign->id;
+                            $newaud->ads_id = $newads->id;
+                            $newaud->fe_id = $audcheck->fe_id;
+                            $newaud->selected_fe_id = $ads->audience_id;
+                            $newaud->name = $audcheck->name;
+                            $newaud->description = $audcheck->description;
+                            $newaud->price = $audcheck->price;
+                            $newaud->price_airdrop = $audcheck->price_airdrop;
+                            $newaud->total_user = $audcheck->total_user;
+                            $newaud->save();
+                        }
                     }
 
 
