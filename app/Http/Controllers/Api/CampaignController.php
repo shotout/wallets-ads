43c5fff740d8251dd086d9b974c9cec2086ee205
shotout <hevaldo66@gmail.com>
2023-04-03 10:34:59 +0700

    adjust ad headlines

--- app/Http/Controllers/Api/CampaignController.php
+++ app/Http/Controllers/Api/CampaignController.php
@@ -320,7 +320,7 @@ class CampaignController extends Controller
                     $newAds = new Ads;
                     $newAds->campaign_id = $campaign->id;
                     if (isset($ads->headlines)) {
-                        $newAds->headlines = $ads->headlines;
+                        $newAds->name = $ads->headlines;
                     }
                     if (isset($ads->description)) {
                         $newAds->description = $ads->description;
